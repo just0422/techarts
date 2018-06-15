7380b6a3cb0ad5927231972f32a6b9736f8e091d
Justin <just0422@gmail.com>
2018-06-15 12:42:31 -0400

    Colors change when checkbox is clicked

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -7,17 +7,43 @@ export default class Question extends Component {
         super();
 
         this.handleCheck = this.handleCheck.bind(this);
+        this.stopPropogation = this.stopPropogation.bind(this);
+
+        this.state = {
+            checked : false,
+            checkedColor: "incomplete"
+        }
     }
 
-    handleCheck(){
+    handleCheck(event){
+        event.stopPropagation(); // Should stop bubble slicks
+        event.nativeEvent.stopImmediatePropagation();
+        console.log("Once?");
+
+        if (this.state.checked){
+            this.setState({ 
+                checked : false,
+                checkedColor: "incomplete"
+            })
+        }
+        else{
+            this.setState({ 
+                checked : true,
+                checkedColor: "complete"
+            })
+        }
+    }
+
+    stopPropogation(event){
+        event.stopPropagation(); // Should prevent bubble slicks
     }
 
     render(){
         return (
-            <div className="question">
-                <label className="control control-checkbox">
+            <div className={['question', 'question-'+this.state.checkedColor].join(' ')}>
+                <label className="control control-checkbox" onClick={this.handleCheck}>
 					{ this.props.question_text }
-					<input type="checkbox" />
+					<input type="checkbox" value={this.state.checked} onClick={this.stopPropogation} />
 					<div className="control_indicator"></div>
 				</label> 
 			</div>
