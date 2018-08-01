4d9038fd2b7d401ec38fb7f558992b2be2c83a22
Justin <just0422@gmail.com>
2018-08-01 15:10:21 -0400

    Began admin page for all but checklist

--- checklist/admin.py
+++ checklist/admin.py
@@ -1,3 +1,32 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+TEAMS = (
+    ('audio', 'Audio'),
+    ('lighting', 'Lighting'),
+    ('media', 'Media'),
+    ('stage crew', 'Stage Crew'),
+    ('video', 'Video')
+)
+CAMPUSES = (
+    ('CTG', 'CTG'),
+    ('CTB', 'CTB')
+)
+
+class TeamAdminForm(forms.ModelForm):
+    campus=forms.ChoiceField(widget=forms.Select, choices=CAMPUSES)
+    team_name=forms.ChoiceField(widget=forms.Select, choices=TEAMS)
+ 
+class TeamAdmin(admin.ModelAdmin):
+    fields = ['team_name', 'campus']
+    list_display = ('campus', 'team_name')
+    list_filter = ['campus', 'team_name']
+    form=TeamAdminForm
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Section)
+admin.site.register(Question)
+admin.site.register(SubQuestion)
+admin.site.register(Fixture)
