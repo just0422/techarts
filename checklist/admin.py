4cbbe831ba49a237ef0b20e847b65062e8430b90
Justin <just0422@gmail.com>
2018-08-01 15:30:45 -0400

    Basic Admin setup

--- checklist/admin.py
+++ checklist/admin.py
@@ -21,12 +21,39 @@ class TeamAdminForm(forms.ModelForm):
  
 class TeamAdmin(admin.ModelAdmin):
     fields = ['team_name', 'campus']
-    list_display = ('campus', 'team_name')
+    list_display = ['campus', 'team_name']
     list_filter = ['campus', 'team_name']
+    ordering = ['campus', 'team_name']
     form=TeamAdminForm
 
+class SectionAdmin(admin.ModelAdmin):
+    fields = ['section_name', 'page_number', 'team']
+    list_display = ['section_name', 'page_number', 'team']
+    list_filter = ['team']
+    ordering = ['team', 'page_number']
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['question_text', 'question_number', 'section', 'team']
+    list_display = ['question_text', 'section', 'question_number', 'team']
+    list_filter = ['section', 'team']
+    ordering = ['team', 'section', 'question_number']
+    
+class SubQuestionAdmin(admin.ModelAdmin):
+    fields = ['title', 'category', 'categoryId', 'question']
+    list_display = ['title', 'category', 'categoryId', 'question']
+    list_filter = ['category', 'question']
+    ordering = ['question', 'category']
+
+class FixtureAdmin(admin.ModelAdmin):
+    fields = ['name', 'channel', 'group', 'campus', 'active', 'working', 'reason']
+    list_display = ['name', 'channel', 'group', 'campus', 'active', 'working', 'reason']
+    list_filter = ['group', 'campus', 'active', 'working']
+    ordering = ['campus', 'channel']
+    
+
+
 admin.site.register(Team, TeamAdmin)
-admin.site.register(Section)
-admin.site.register(Question)
-admin.site.register(SubQuestion)
-admin.site.register(Fixture)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(SubQuestion, SubQuestionAdmin)
+admin.site.register(Fixture, FixtureAdmin)
